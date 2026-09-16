c910c6b6285875b23bc22641d447b31e7f8cc606
Shefa Akter <shefaakter2211@gmail.com>
2026-09-16 09:16:35 +0600

    Add complete Flask app structure with authentication system

---
+++ config.py
@@ -0,0 +1,4 @@
+﻿class Config:
+    SECRET_KEY = 'change-this-to-something-random-later'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/bike_marketplace_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
